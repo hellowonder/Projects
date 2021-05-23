d8db8c2b03a3070cb5f91544cb6d76642946d36b
Shubham <shubham30898@gmail.com>
2021-05-23 23:11:01 +0530

    changes in age limit

--- CowinPune/app.js
+++ CowinPune/app.js
@@ -15,7 +15,7 @@ fetch(`https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistri
     // console.log(parseData);
     
     for(let listOfHospital of parseData.sessions){
-        if(listOfHospital.avilable_capacity > 0){
+        if((listOfHospital.available_capacity_dose1 > 0) || (listOfHospital.available_capacity_dose2 > 0)){
 
             let name = document.createElement('p')
             name.innerHTML = `Hospital Name: ${listOfHospital.name}`
@@ -33,9 +33,21 @@ fetch(`https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistri
             vaccine.innerHTML = `Vaccine: ${listOfHospital.vaccine}`
             ul.append(vaccine)
 
+            let available_capacity_dose1 = document.createElement('p')
+            available_capacity_dose1.innerHTML = `Capacity Dose1: ${listOfHospital.available_capacity_dose1}`
+            ul.append(available_capacity_dose1)
+
+            let available_capacity_dose2 = document.createElement('p')
+            available_capacity_dose2.innerHTML = `Capacity Dose2: ${listOfHospital.available_capacity_dose2}`
+            ul.append(available_capacity_dose2)
+
             let avilable_capacity = document.createElement('p')
             avilable_capacity.innerHTML = `No of Avilable Shots: ${listOfHospital.avilable_capacity}`
             ul.append(avilable_capacity)
+
+            let min_age_limit = document.createElement('p')
+            min_age_limit.innerHTML = `Minimum Age Limit: ${listOfHospital.min_age_limit}`
+            ul.append(min_age_limit)
 
             let slots = document.createElement('p')
             slots.innerHTML = `Slots: ${listOfHospital.slots}`
